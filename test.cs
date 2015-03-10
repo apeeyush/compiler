7ddb010fd22512dbf158dc23b1703743607f58a8
apeeyush <peeyushagarwal1994@gmail.com>
2015-03-10 19:17:29 +0530

    Add test file

---
+++ test.cs
@@ -0,0 +1,14 @@
+namespace Program
+{
+	class HelloWorld
+	{
+		public int a = 6;
+		static void Main(string[] args)
+		{
+			/*my first program in C#*/
+			Console.WriteLine("Hello World\n");
+			Console.ReadKey();
+		}
+
+	}
+}
